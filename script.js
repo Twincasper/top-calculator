fe20fe37732b592fdfd55ba0aff2d3a45fd639fc
Justin Watson <61891187+Twincasper@users.noreply.github.com>
2024-09-19 13:59:29 -0400

    Established variables in script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let num1;
+let num2;
+let operator;
+
+// Need add, subtract, multiply, and divide operations
+
+// Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions(add, subtract, multiply, divide) on the numbers.
